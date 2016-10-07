9afa982b972e4925f3738b4747c00a33ea19444c
f14kyle <f14kyle.work@gmail.com>
2016-10-07 11:12:26 -0700

    Finished first iteration of differential scraper (OHLC_scraper)
    - File existence and last scraped date comparator performed using by
    reading the original data itself rather than some itermediary log file
    that records the data.  Although the number of I/O operations may have
    increased, the code is simpler, and I don't have to reply on some log
    file that could be tampered with.

--- OHLC_scraper.R
+++ OHLC_scraper.R
@@ -4,9 +4,10 @@
 # ETF Metadata Scraper
 # This program scrapes ETF metadata from ETFDB
 
-rm(list=ls()) 
+rm(list=ls())
 library(readxl)
 library(quantmod)
+library(R.utils) # for countlines
 
 directory = "D:/Github/DataDrivenTrading/"
 data_path = "D:/Github/DataDrivenTrading/Data/OHLC/"
@@ -14,74 +15,73 @@ data_path = "D:/Github/DataDrivenTrading/Data/OHLC/"
 setwd(directory)
 
 # Reads in last update information
-df.existing = read.table("log.csv",header = TRUE,sep = ",", stringsAsFactors=F)
-  
-# Load list of target instruments
-df.target <- data.frame(read_excel("etfdb_data.xls"))
-colnames(df.target) = c("symbol","name","etfdb.category","inception.date","expense.ratio","commission.free","expenses.rating")
+#df.log = read.table("log.csv",header = TRUE,sep = ",", stringsAsFactors=F)
 
-# Create a dataframe that contains information on what data to fetch
-df.comparison = data.frame("target.symbol" = df.target$symbol,
-                           "target.exists" = rep(NA,nrow(df.target)),
-                           "target.updated" = rep(NA,nrow(df.target)),
-                           stringsAsFactors=FALSE)
+# Load list of target instruments
+df.source <- data.frame(read_excel("etfdb_data.xls"))
+
+# Delete all irrelevant columns
+df.target = within(df.source,rm(name,etfdb.category,inception.date,expense.ratio,commission.free,expenses.rating))
+
+# Create another column to keep track of its existence in folder
+df.target["exists"] = rep(NA,nrow(df.target))
+
+# Check folder to determine which instruments have been scraped
+existing.files = list.files("./Data/OHLC",full.names = FALSE) # fetch list of all files from folder
+existing.files = gsub(".csv","",existing.files) # remove ".csv" from vector of characters
 
 # Perform comparisons to determine if target instrument exists and if it is updated
-for (i in 1:nrow(df.comparison)){
-  df.comparison$target.exists[i] = df.target$symbol[i] %in% df.existing$fetched.symbol
-  df.comparison$target.updated[i] = Sys.Date() == df.existing$fetched.date[grep(df.target$symbol[100],df.existing$fetched.symbol)]
+for (i in 1:nrow(df.target)){
+  df.target$exists[i] = df.target$symbol[i] %in% existing.files
 }
-
-
-# Check file checker to determine which instruments need to be updated
-#existing.files = list.files("./Data/OHLC",full.names = FALSE) # fetch list of all files from folder
-#existing.files = gsub(".csv","",existing.files) # remove ".csv" from vector of characters
-
 
 # Recreate log
 df.log = data.frame("fetched.symbol" = character(),
-                    "fetched.date" = as.Date(character()),
-                    "fetched.status" = character(),
-                    stringsAsFactors=FALSE)
+                   "fetched.date" = as.Date(character()),
+                  "fetched.status" = character(),
+                 stringsAsFactors=FALSE)
 
 # For each symbol among the target symbols, perform teh following action according to the situation:
 
 # If target exists and the target is updated, do nothing
 # If target exists and the target is not updated, append data to existing CSV from last fetched date
 # If target does not exist, scrape
-for (i in 1:nrow(df.comparison)){
+for (i in 1:nrow(df.target)){
   tryCatch({
-    target.exists = df.comparison$target.exists[i]
-    target.updated = df.comparison$target.updated[i]
+    ticker = df.target$symbol[i]
+    target.exists = df.target$exists[i]
+    file_path = paste(data_path,ticker,".csv",sep = "")
+    skipLines = countLines(file_path)
+    date.fetched = try(as.Date(as.matrix(read.csv(file_path,nrows = 1,skip = skipLines-1, header = FALSE)[1])))
     
-    if (target.exists == TRUE & target.updated == FALSE){
-      startDate = df.existing$fetched.date[grep(df.target$symbol[i],df.existing$fetched.symbol)]
-      ticker = df.comparison$target.symbol[i]
+    if (target.exists == TRUE & (Sys.Date() - 1) != date.fetched){
+      startDate = date.fetched
       data = getSymbols(Symbols = ticker,
                         src = "yahoo",
                         from = startDate,
                         auto.assign = FALSE)
-      write.table(data,paste(data_path,ticker,".csv",sep = ""), append = TRUE, sep = ",")
-      df.log[nrow(df.log) + 1,] =c(i,as.character(Sys.Date()),"success")
+      write.zoo(data,paste(data_path,ticker,".csv",sep = ""), append = TRUE, sep = ",",col.names = FALSE)
+      #df.log[nrow(df.log) + 1,] =c(df.target$symbol[i],as.character(Sys.Date()),"success")
     }
     
     if (target.exists == FALSE){
-      ticker = df.comparison$target.symbol[i]
+      ticker = df.target$symbol[i]
       data = getSymbols(Symbols = ticker,
                         src = "yahoo",
-                        auto.assign = FALSE) 
+                        auto.assign = FALSE)
       colnames(data) = c("open","high","low","close","volume","adjusted")
       write.zoo(data,paste(data_path,ticker,".csv",sep = ""),sep=",",row.names=FALSE) # sep = "" to get rid of space in paste operations
       
       # when successful write status to log
-      df.log[nrow(df.log) + 1,] =c(i,as.character(Sys.Date()),"success")
+      #df.log[nrow(df.log) + 1,] =c(df.target$symbol[i],as.character(Sys.Date()),"success")
     }
-
-          }, 
-  # when there is an error, write error  
+    
+  },
+  # when there is an error, write error 
   error = function(e) {
-    df.log[nrow(df.log) + 1,] =c(i,as.character(Sys.Date()),"failure")
+    df.log[nrow(df.log) + 1,] =c(df.target$symbol[i],as.character(Sys.Date()),"failure")
   })
 }
 
-write.csv(df.log,file = "log.csv",row.names=FALSE)
+
+# write.csv(df.log,file = "log.csv",row.names=FALSE)
--- etfdb_data_scraper.R
+++ etfdb_data_scraper.R
@@ -3,14 +3,17 @@
 # This program scrapes ETF metadata from ETFDB
 
 rm(list=ls()) 
-directory = "D:/Github/DataDrivenTrading"
+directory = "C:/userdata/DataDrivenTrading"
 setwd(directory)
 
 # Load libraries
 library(readxl)
 library(quantmod)
-library(rvest) # for read_html
+#library(rvest) # for read_html
 library(rPref)
+library(ggplot2)
+library(ggrepel)
+library(scales)
 
 # Load list of ETFs
 # Source: http://www.nasdaq.com/etfs/list
@@ -23,8 +26,18 @@ colnames(df) = c("symbol","name","etfdb.category","inception.date","expense.rati
 # Correct Excel format date for inception date
 df$inception.date = as.Date(df$inception.date,origin = "1899-12-30")
 
+df["c.issuer"] = NA
+
 # Load latest set of scraped sites from ETFdb.com
-load("20160930_allsites.rda")
+#load("20160930_allsites.rda")
+
+# Create requisite columns
+df["c.issuer"] = gsub( " .*$", "", df$name )
+df["c.structure"] = NA
+df["c.expense.ratio"] = NA
+df["c.inception.date"] = NA
+df["c.tax.form"] = NA
+df["c.tracking.index"] = NA
 
 n.top = 10
 
@@ -39,113 +52,154 @@ df.scb = psel(subset(df,etfdb.category == "Small Cap Blend Equities"),low(expens
 df.scg = psel(subset(df,etfdb.category == "Small Cap Growth Equities"),low(expense.ratio),top = n.top)
 df.scv = psel(subset(df,etfdb.category == "Small Cap Value Equities"),low(expense.ratio),top = n.top)
 
-# Save all dividend data to list
-OHLC.data = vector("list",nrow(df))
 
-for (i in 1:nrow(df)){
-  OHLC.data[[i]] = try(getSymbols(df$symbol[i], src = "yahoo"))
-}
+# Plot box-whisker for each category
 
-
-
-
-
-
-
-
-
-
-plot.allcap = ggplot() + 
-  geom_point(data = df.ac,aes(x = c.issuer,y = expense.ratio)) + 
-  geom_text(data = df.ac,aes(x = c.issuer,y = expense.ratio,label = df.ac$symbol),hjust=0, vjust=1) + 
+plot.box = ggplot(df,aes(x = etfdb.category,y = expense.ratio)) + 
+  geom_boxplot(outlier.colour = "black", outlier.size = ) +
+  geom_jitter(aes(colour = etfdb.category)) + 
+  scale_y_continuous(labels = percent,breaks = scales::pretty_breaks(n = 10)) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))  
+
+
+# Plot cheapest 10 ETFs per category
+plot.allcap = ggplot() + 
+  geom_point(data = df.ac,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.ac,aes(x = c.issuer,y = expense.ratio,label = df.ac$symbol)) + 
+  scale_y_continuous(labels = percent) + 
+  theme(legend.title=element_blank(),
+        legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
 plot.lcb = ggplot() + 
   geom_point(data = df.lcb,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.lcb,aes(x = c.issuer,y = expense.ratio,label = df.lcb$symbol)) + 
+  ggtitle("Large Cap Blend") +
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
 plot.lcg = ggplot() + 
   geom_point(data = df.lcg,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.lcg,aes(x = c.issuer,y = expense.ratio,label = df.lcg$symbol)) + 
+  ggtitle("Large Cap Growth") +
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
 plot.lcv = ggplot() + 
   geom_point(data = df.lcv,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.lcv,aes(x = c.issuer,y = expense.ratio,label = df.lcv$symbol)) + 
+  ggtitle("Large Cap Value") + 
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
 plot.mcb = ggplot() + 
   geom_point(data = df.mcb,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.mcb,aes(x = c.issuer,y = expense.ratio,label = df.mcb$symbol)) + 
+  ggtitle("Small Cap Blend") + 
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
 plot.mcg = ggplot() + 
   geom_point(data = df.mcg,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.mcg,aes(x = c.issuer,y = expense.ratio,label = df.mcg$symbol)) + 
+  ggtitle("Mid Cap Growth") + 
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
 plot.mcv = ggplot() + 
-  geom_point(data = df.mcv,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_point(data = df.mcv,aes(x = c.issuer,y = expense.ratio)) +
+  geom_text_repel(data = df.mcv,aes(x = c.issuer,y = expense.ratio,label = df.mcv$symbol)) + 
+  ggtitle("Mid Cap Value") + 
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
+  
 plot.scb = ggplot() + 
   geom_point(data = df.scb,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.scb,aes(x = c.issuer,y = expense.ratio,label = df.scb$symbol)) + 
+  ggtitle("Small Cap Blend") + 
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
-
+  
+  
 plot.scg = ggplot() + 
   geom_point(data = df.scg,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.scg,aes(x = c.issuer,y = expense.ratio,label = df.scg$symbol)) + 
+  ggtitle("Small Cap Growth") + 
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
 plot.scv = ggplot() + 
   geom_point(data = df.scv,aes(x = c.issuer,y = expense.ratio)) + 
+  geom_text_repel(data = df.scv,aes(x = c.issuer,y = expense.ratio,label = df.scv$symbol)) + 
+  ggtitle("Small Cap Value") + 
+  scale_y_continuous(labels = percent) + 
   theme(legend.title=element_blank(),
         legend.position = "top",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.3))
 
+grid.arrange(plot.lcv,plot.lcb,plot.lcg,plot.mcv,plot.mcb,plot.mcg,plot.scv,plot.scb,plot.scg,ncol = 3)
 
-# Create requisite columns
-df["c.issuer"] = gsub( " .*$", "", df$name )
-df["c.structure"] = NA
-df["c.expense.ratio"] = NA
-df["c.inception.date"] = NA
-df["c.tax.form"] = NA
-df["c.tracking.index"] = NA
-
-
-for (i in 1:nrow(df)){
-  cast <- html_nodes(all.sites[[i]], ".pull-right")
-  df$c.issuer[i] = html_text(cast)[6]
-  df$c.structure[i] = html_text(cast)[7]
-  df$c.expense.ratio[i] = html_text(cast)[8]
-  df$c.inception.date[i] = html_text(cast)[10]
-  df$c.tax.form[i] = html_text(cast)[11]
-  df$c.tracking.index[i] = html_text(cast)[12]
-}
+#for (i in 1:nrow(df)){
+#  cast <- html_nodes(all.sites[[i]], ".pull-right")
+#  df$c.issuer[i] = html_text(cast)[6]
+#  df$c.structure[i] = html_text(cast)[7]
+#  df$c.expense.ratio[i] = html_text(cast)[8]
+#  df$c.inception.date[i] = html_text(cast)[10]
+#  df$c.tax.form[i] = html_text(cast)[11]
+#  df$c.tracking.index[i] = html_text(cast)[12]
+#}
 
 
-for (i in 1:nrow(df)){
-  cast <- try(html_nodes(all.sites[[i]], "span"))
-  df$c.issuer[i] = try(html_text(cast)[6])
-  df$c.structure[i] = try(html_text(cast)[7])
-  df$c.expense.ratio[i] = try(html_text(cast)[8])
-  df$c.inception.date[i] = try(html_text(cast)[10])
-  df$c.tax.form[i] = try(html_text(cast)[11])
-  df$c.tracking.index[i] = try(html_text(cast)[12])
-}
+#for (i in 1:nrow(df)){
+  #cast <- try(html_nodes(all.sites[[i]], "span"))
+  #df$c.issuer[i] = try(html_text(cast)[6])
+  #df$c.structure[i] = try(html_text(cast)[7])
+##  df$c.expense.ratio[i] = try(html_text(cast)[8])
+#  df$c.inception.date[i] = try(html_text(cast)[10])
+#  df$c.tax.form[i] = try(html_text(cast)[11])
+#  df$c.tracking.index[i] = try(html_text(cast)[12])
+#}
 
 
 
