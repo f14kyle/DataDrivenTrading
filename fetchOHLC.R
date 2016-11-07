a051bf692401f219db73fd1d329df6a93aa99106
f14kyle.work@gmail.com <f14kyle.work@gmail.com>
2016-11-07 13:13:20 -0700

    Fixed conversion from dataframe to xts
    My original code to coerce a dataframe into an its read as follows:
    xts(x = data,order.by = as.Date(data$Index)) where data is a dataframe
    read from a csv.  I changed it to xts(x = data[,-1],order.by =
    as.Date(data$Index)).
    
    Make sure to include the [,-1] in order to avoid the date column when
    constructing the xts.  The xts file structure is inherently a matrix,
    which assumes one data type.  If dates and numerics are mixed, they
    will all be coerced into characters, which will lead to problems down
    the road.  One potential problem is using Return.calculate to calculate
    daily returns, which assumes numeric values.

--- fetchOHLC.R
+++ fetchOHLC.R
@@ -7,7 +7,8 @@ all.stocks.list = vector("list",nrow(df.target))
 # Save all target stocks into list.  The list will also include the failures.
 for (i in 1:nrow(df.target)){
   data = try(read.csv(paste(data_path,df.target$symbol[i],".csv",sep = ""),header = TRUE,sep = ",",stringsAsFactors = FALSE))
-  all.stocks.list[[i]] = try(xts(x = data,order.by = as.Date(data$Index)))
+  # Make sure to include the [,-1] in order to avoid the date column when constructing the xts.  The xts file structure is inherently a matrix, which assumes one data type.  If dates and numerics are mixed, they will all be coerced into characters.
+  all.stocks.list[[i]] = try(xts(x = data[,-1],order.by = as.Date(data$Index)))
 }
 
 
